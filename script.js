414937024aa620da86f4c3a4c4e199dcc0f339d4
Knesello <knesello@fibrick.com>
2025-04-28 10:02:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,15 +13,6 @@ const pairs = [
   ['Xyrah', 'Kenzie']
 ];
 
-// Function to display the pairs
-function displayPairs(pairs) {
-  pairList.innerHTML = ''; // Clear previous list
-  pairs.forEach((pair, index) => {
-    const li = document.createElement('li');
-    li.textContent = `Pair ${index + 1}: ${pair[0]} & ${pair[1]}`;
-    pairList.appendChild(li);
-  });
-}
 
 // Always display the fixed buddy pairs
 displayPairs(pairs);
