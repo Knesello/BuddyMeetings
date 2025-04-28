066007a1781eb4f74c8726be00f12591baf3b9f0
Knesello <knesello@fibrick.com>
2025-04-28 07:30:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Updated team member list: Chris and Donna removed, April added
-let names = ["Kirsten", "Laila", "Joy", "Xyrah", "Princess", "Brianna", "Lovely", "Kenzie", "Janelle"];
+let names = ["Kirsten", "Laila", "Joy", "Xyrah", "Nikki", "Brianna", "Lovely", "Kenzie", "Jodelle", "Flor", "Joyce", "Maria"];
 const startDate = new Date(2024, 8, 23).getTime(); // September 23rd, 2024 (Month is 0-indexed, so 8 is September)
 const pairingPeriod = 14 * 24 * 60 * 60 * 1000; // 2 weeks in milliseconds
 
